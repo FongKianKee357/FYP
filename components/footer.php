e104fa3f9dceea7d48ab2f27d8f63df80a612dbd
TEOKEXIN <1211211375@student.mmu.edu.my>
2024-05-03 10:18:49 +0800

    fblink

--- components/footer.php
+++ components/footer.php
@@ -28,7 +28,7 @@
 
       <div class="box">
          <h3>follow us</h3>
-         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://www.facebook.com/boutiquehandcraftmodelhouse/"><i class="fab fa-facebook-f"></i>facebook</a>
          <a href="#"><i class="fab fa-twitter"></i>twitter</a>
          <a href="#"><i class="fab fa-instagram"></i>instagram</a>
          <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
