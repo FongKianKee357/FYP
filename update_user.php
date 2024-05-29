82f340d93cf528c556f3ad61220285dc65a7b7b2
LOW REN XING <1211210349@student.mmu.edu.my>
2024-05-29 18:39:17 +0800

    low

--- update_user.php
+++ update_user.php
@@ -11,16 +11,33 @@ if(isset($_SESSION['user_id'])){
 };
 
 if (isset($_POST['submit'])) {
-   $name = $_POST['name'];
-   $name = filter_var($name, FILTER_SANITIZE_STRING);
-   $email = $_POST['email'];
-   $email = filter_var($email, FILTER_SANITIZE_STRING);
 
-   $update_profile = $conn->prepare("UPDATE `users` SET name = ?, email = ? WHERE id = ?");
-   if($update_profile->execute([$name, $email, $user_id])){
-      $message[] = 'Profile updated successfully!';
+   if (!empty($_POST['name']) || !empty($_POST['email'])) {
+      $name = $_POST['name'];
+      $name = filter_var($name, FILTER_SANITIZE_STRING);
+      $email = $_POST['email'];
+      $email = filter_var($email, FILTER_SANITIZE_STRING);
+   
+      $update_profile = $conn->prepare("UPDATE `users` SET name = ?, email = ? WHERE id = ?");
+   
+      if($update_profile->execute([$name, $email, $user_id])){
+         $message[] = 'Profile updated successfully!';
+      }
    }
+   
+if (!empty($_POST['phoneNum'])) {
+    $phoneNum = $_POST['phoneNum'];
+    $phoneNum = filter_var($phoneNum, FILTER_SANITIZE_STRING);
 
+   $update_phoneNum = $conn->prepare("UPDATE `users` SET phoneNum = ? WHERE id = ?");
+
+   if ($update_phoneNum->execute([$phoneNum, $user_id])) {
+         $message[] = 'Phone Number updated successfully!';
+   }else{
+               $message[] = 'Error updating phone number!';
+   }
+}
+  
 
    if (!empty($_POST['old_pass']) || !empty($_POST['new_pass']) || !empty($_POST['cpass'])) {
        $empty_pass = 'da39a3ee5e6b4b0d3255bfef95601890afd80709';
@@ -40,8 +57,8 @@ if (isset($_POST['submit'])) {
            $message[] = 'Confirm password not matched!';
        } else {
            if ($new_pass != $empty_pass) {
-               $update_admin_pass = $conn->prepare("UPDATE `users` SET password = ? WHERE id = ?");
-               $update_admin_pass->execute([$cpass, $user_id]);
+               $update_user_pass = $conn->prepare("UPDATE `users` SET password = ? WHERE id = ?");
+               $update_user_pass->execute([$cpass, $user_id]);
                $message[] = 'Password updated successfully!';
            } else {
                $message[] = 'Please enter a new password!';
@@ -97,11 +114,12 @@ if (isset($_POST['submit_a'])) {
             <form action="" method="post">
                <h3>update personal</h3>
                <input type="hidden" name="prev_pass" value="<?= $fetch_profile["password"]; ?>">
-               <input type="text" name="name" required placeholder="enter your username" maxlength="20"  class="box" value="<?= $fetch_profile["name"]; ?>">
-               <input type="email" name="email" required placeholder="enter your email" maxlength="50"  class="box" oninput="this.value = this.value.replace(/\s/g, '')" value="<?= $fetch_profile["email"]; ?>">
-               <input type="password" name="old_pass" placeholder="enter your old password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-               <input type="password" name="new_pass" placeholder="enter your new password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
-               <input type="password" name="cpass" placeholder="confirm your new password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+               <input type="text" name="name" required placeholder="Enter your Username" maxlength="20"  class="box" value="<?= $fetch_profile["name"]; ?>">
+               <input type="email" name="email" required placeholder="Enter your Email" maxlength="50"  class="box" oninput="this.value = this.value.replace(/\s/g, '')" value="<?= $fetch_profile["email"]; ?>">
+               <input type="num" name="phoneNum" required placeholder="Enter your Phone Number" maxlength="15"  class="box" oninput="this.value = this.value.replace(/\s/g, '')" value="<?= $fetch_profile["phoneNum"]; ?>">
+               <input type="password" name="old_pass" placeholder="Enter your Old Password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+               <input type="password" name="new_pass" placeholder="Enter your New Password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+               <input type="password" name="cpass" placeholder="Confirm your New Password" maxlength="20"  class="box" oninput="this.value = this.value.replace(/\s/g, '')">
                <input type="submit" value="update now" class="btn" name="submit">
             </form>
          </div>
@@ -109,9 +127,9 @@ if (isset($_POST['submit_a'])) {
          <div id="address" class="section" style="display: none;">
             <form action="" method="post">
                <h3>update address</h3>
-               <input type="text" name="address1" placeholder="Enter your address 1" maxlength="100" class="box" value="<?= $fetch_profile["address1"];?>" >
-               <input type="text" name="address2" placeholder="Enter your address 2" maxlength="100" class="box" value="<?= $fetch_profile["address2"];?>">
-               <input type="text" name="address3" placeholder="Enter your address 3" maxlength="100" class="box" value="<?= $fetch_profile["address3"];?>">
+               <input type="text" name="address1" placeholder="Enter your Address 1" maxlength="100" class="box" value="<?= $fetch_profile["address1"];?>" >
+               <input type="text" name="address2" placeholder="Enter your Address 2" maxlength="100" class="box" value="<?= $fetch_profile["address2"];?>">
+               <input type="text" name="address3" placeholder="Enter your Address 3" maxlength="100" class="box" value="<?= $fetch_profile["address3"];?>">
                <input type="submit" value="update now" class="btn" name="submit_a">
             </form>
          </div>
